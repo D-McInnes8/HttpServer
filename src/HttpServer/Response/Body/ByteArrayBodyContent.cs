21f9f1862017abc01e85ab4f682c2d619ccd9767
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2025-01-25 19:29:06 +1000

    Added the ByteArrayBodyContent.cs class.

---
+++ src/HttpServer/Response/Body/ByteArrayBodyContent.cs
@@ -0,0 +1,37 @@
+using System.Text;
+
+namespace HttpServer.Response.Body;
+
+/// <summary>
+/// Represents a byte array body for a HTTP response.
+/// </summary>
+public class ByteArrayBodyContent : IHttpBodyContent
+{
+    /// <summary>
+    /// Constructs a new <see cref="ByteArrayBodyContent"/> with the specified content.
+    /// </summary>
+    /// <param name="content"></param>
+    /// <param name="contentType"></param>
+    /// <param name="encoding"></param>
+    public ByteArrayBodyContent(byte[] content, HttpContentType contentType, Encoding encoding)
+    {
+        Content = content;
+        ContentType = contentType;
+        Encoding = encoding;
+    }
+    
+    /// <inheritdoc />
+    public HttpContentType ContentType { get; }
+    
+    /// <inheritdoc />
+    public Encoding Encoding { get; }
+    
+    /// <inheritdoc />
+    public byte[] Content { get; }
+    
+    /// <inheritdoc />
+    public void CopyTo(Span<byte> destination)
+    {
+        Content.CopyTo(destination);
+    }
+}
