434b68607e34f91085f60043f667ad189901f539
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2025-02-09 16:05:02 +1000

    Cleaned up TcpNetworkStreamReader.cs code.

--- src/HttpServer/Networking/TcpNetworkStreamReader.cs
+++ src/HttpServer/Networking/TcpNetworkStreamReader.cs
@@ -11,7 +11,7 @@ internal class TcpNetworkStreamReader : IDisposable
 {
     private readonly TcpClientConnection _tcpClientConnection;
     private readonly Encoding _encoding;
-    private bool _bufferFilled;
+    private bool _bufferInitialised;
     private bool _isDisposed;
     
     private readonly byte[] _buffer;
@@ -33,17 +33,6 @@ internal class TcpNetworkStreamReader : IDisposable
         _buffer = BufferPool.Rent(BufferSize);
         _bufferPosition = 0;
         _bufferLength = 0;
-    }
-
-    public async Task ListenToStream()
-    {
-        while (await FillBufferAsync() > 0)
-        {
-            if (_isDisposed)
-            {
-                return;
-            }
-        }
     }
 
     /// <summary>
@@ -116,48 +105,38 @@ internal class TcpNetworkStreamReader : IDisposable
                 break;
             }
         } while (await FillBufferAsync() > 0);
-        /*while (await FillBufferAsync() > 0)
-        {
-            var span = _buffer.AsSpan(_bufferPosition);
-            var bytesToRead = Math.Min(remaining, span.Length);
-            
-            buffer.Append(_encoding.GetString(span[..bytesToRead]));
-            _bufferPosition += bytesToRead;
-            
-            remaining -= bytesToRead;
-            if (remaining == 0)
-            {
-                break;
-            }
-        }*/
 
         Debug.Assert(remaining == 0);
         return buffer.ToString();
     }
     
+    /// <summary>
+    /// Initialises the buffer by filling it with data from the stream.
+    /// </summary>
+    /// <returns></returns>
     private Task InitialiseBufferAsync()
     {
-        if (_bufferFilled)
+        if (_bufferInitialised)
         {
             return Task.CompletedTask;
         }
 
-        _bufferFilled = true;
+        _bufferInitialised = true;
         return FillBufferAsync();
     }
     
+    /// <summary>
+    /// Fills the buffer with data from the stream.
+    /// </summary>
+    /// <returns>The number of bytes read from the stream.</returns>
     private async Task<int> FillBufferAsync()
     {
-        /*if (_bufferPosition < _bufferLength)
-        {
-            return _bufferLength;
-        }*/
-        
         _bufferLength = await _tcpClientConnection.Stream.ReadAsync(_buffer, 0, BufferSize);
         _bufferPosition = 0;
         return _bufferLength;
     }
 
+    /// <inheritdoc />
     public void Dispose()
     {
         _isDisposed = true;
